b4f65ca822f2f5290dbb88cf626e44452aafde98
giahuytruonn <zblackz1oooo@gmail.com>
2025-09-17 16:57:37 +0700

    Done Navigate Search Page ( No UI )

--- src/components/ToolBarComponent.tsx
+++ src/components/ToolBarComponent.tsx
@@ -1,75 +1,79 @@
 import React from "react";
-import {View, TextInput, StyleSheet, Image} from 'react-native'
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Image,
+  Pressable,
+  Text,
+} from "react-native";
 import { colors } from "../constants/colors";
+import { useNavigation } from "@react-navigation/native";
 
 interface toolProps {
-    bill?: boolean,
+  bill?: boolean;
 }
 
-export const ToolBarComponent: React.FC<toolProps> = ({
-    bill = false,
-}) => {
-        return (
-            <View style={styles.container}>
-                <View style={styles.input}>
-                    <Image 
-                        source={require('../../assets/searchIcon.png')} 
-                        style={{marginRight: 3}}
-                    />
-                    <TextInput
-                        placeholder="Super sale 10.10 with 30% dea..."
-                        
-                    />
-                </View>
-                <View style={styles.tools}>
-                    <Image 
-                        source={require('../../assets/noti.png')} 
-                        style={styles.icon}
-                    />
-                    <Image 
-                        source={require('../../assets/chat.png')} 
-                        style={styles.icon}
-                    />
-                    {bill && (
-                        <Image 
-                            source={require('../../assets/purchase.png')} 
-                            style={styles.icon}
-                        />
-                    )}
-                </View>
-            </View>
-        )
-}
+export const ToolBarComponent: React.FC<toolProps> = ({ bill = false }) => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Pressable
+        onPress={() => navigation.navigate("Search" as never)}
+        style={styles.input}
+      >
+        <Image
+          source={require("../../assets/searchIcon.png")}
+          style={{ marginRight: 3 }}
+        />
+        <Text style={{ color: "#8A8A8A" }}>
+          Super sale 10.10 with 30% dea...
+        </Text>
+      </Pressable>
+      <View style={styles.tools}>
+        <Image source={require("../../assets/noti.png")} style={styles.icon} />
+        <Image source={require("../../assets/chat.png")} style={styles.icon} />
+        {bill && (
+          <Image
+            source={require("../../assets/purchase.png")}
+            style={styles.icon}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: colors.themeGra1,
-        paddingVertical: 20,
-        paddingHorizontal: 12,
-    },
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: colors.themeGra1,
+    paddingVertical: 20,
+    paddingHorizontal: 12,
+  },
 
-    input: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: colors.white,
-        borderRadius: 20,
-        flex: 1,
-        paddingHorizontal: 12,
-        // paddingVertical: 3,
-        marginRight: 10,
-    },
+  input: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: colors.white,
+    borderRadius: 20,
+    flex: 1,
+    paddingHorizontal: 12,
+    // paddingVertical: 3,
+    marginRight: 10,
+    height: 50,
+  },
 
-    tools: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
+  tools: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
 
-    icon: {
-        width: 24,
-        height: 24,
-        resizeMode: 'contain',
-        marginLeft: 16,
-    }
-})
+  icon: {
+    width: 24,
+    height: 24,
+    resizeMode: "contain",
+    marginLeft: 16,
+  },
+});
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,52 +1,61 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack"
-import React from "react"
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
 import { ToolBarComponent } from "../components/ToolBarComponent";
 import { HomeScreen } from "../screens/HomeScreen";
 import { SplashScreen } from "../screens/SplashScreen";
 import { BottomTabs } from "./BottomTabs";
+import { SearchScreen } from "../screens/SearchScreen";
 
 export type RootStackParamList = {
-    Splash: undefined,
-    Home: undefined,
-    Main: undefined
-}
+  Splash: undefined;
+  Home: undefined;
+  Main: undefined;
+  Search: undefined;
+};
 
 const Stack = createStackNavigator();
 
-export const RootNavigator :React.FC = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName="Splash"
-                screenOptions={{headerShown: true}}
-                // screenOptions={{
-                //     header: () => <ToolBarComponent bill={false}/>, // default
-                // }}
-            >
-                <Stack.Screen 
-                    name="Splash" 
-                    component={SplashScreen}
-                    options={{
-                        headerShown: false
-                    }}
-                />
-                <Stack.Screen 
-                    name="Home" 
-                    component={HomeScreen}
-                    options={{
-                        header: () => <ToolBarComponent bill={true}/>
-                    }}
-                />
+export const RootNavigator: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{ headerShown: true }}
+        // screenOptions={{
+        //     header: () => <ToolBarComponent bill={false}/>, // default
+        // }}
+      >
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            header: () => <ToolBarComponent bill={true} />,
+          }}
+        />
 
-                <Stack.Screen   
-                    name="Main" 
-                    component={BottomTabs} 
-                    options={{
-                        headerShown: false   // ẩn header mặc định
-                    }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+        <Stack.Screen
+          name="Main"
+          component={BottomTabs}
+          options={{
+            headerShown: false, // ẩn header mặc định
+          }}
+        />
+        <Stack.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
